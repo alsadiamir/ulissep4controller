1b2a178dcbf9ed81bf4a60b1b2eb0ed0e4115602
Riccardo <ricky.roveri@gmail.com>
2022-05-27 18:14:00 +0200

    removed unused code

--- controller/pkg/signals/doc.go
+++
@@ -1,3 +0,0 @@
-// Package signals contains utilities for dealing with signals.
-// This code was taken from https://github.com/kubernetes/sample-controller/tree/master/pkg/signals.
-package signals
